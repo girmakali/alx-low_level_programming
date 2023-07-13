e466838e555fc4b2d0fc28b4bdf6a5c6fe6ba8e5
girmakali <mogesgirma44@yahoo.com>
2023-07-13 12:10:58 -0700

    paragraph

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-/*input output variable*/
-int main(void)/*the main body*/
+/**
+  * main - Entry point
+  *
+  * Return:Always 0 (success)
+  */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
