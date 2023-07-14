c26debc0f64eb41ac77d09476b09df7ecb53f987
girmakali <mogesgirma44@yahoo.com>
2023-07-14 06:10:26 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+  *main - Entry point
+  *Return: Always 0 (succcess)
+  */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	return (0);
+}
