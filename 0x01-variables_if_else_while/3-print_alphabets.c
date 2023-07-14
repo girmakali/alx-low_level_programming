2c6b0ec8923d3c0392eb5141a67fe31c317bd035
girmakali <mogesgirma44@yahoo.com>
2023-07-14 11:46:45 -0700

    upper and lower

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+  *main - Entry point
+  *Return: Always 0 (succcess)
+  */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
